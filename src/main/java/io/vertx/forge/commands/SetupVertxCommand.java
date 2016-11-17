090ae4255c7c6702c193c9cdaa911dbd89c53853
George Gastaldi <gegastaldi@gmail.com>
2016-11-17 13:46:50 -0200

    Fixes #7: Introduces a Vert.x project type

--- src/main/java/io/vertx/forge/commands/SetupVertxCommand.java
+++ src/main/java/io/vertx/forge/commands/SetupVertxCommand.java
@@ -1,5 +1,9 @@
 package io.vertx.forge.commands;
 
+import static java.util.Arrays.asList;
+
+import javax.inject.Inject;
+
 import org.jboss.forge.addon.ui.context.UIBuilder;
 import org.jboss.forge.addon.ui.context.UIExecutionContext;
 import org.jboss.forge.addon.ui.hints.InputType;
@@ -8,38 +12,34 @@ import org.jboss.forge.addon.ui.metadata.WithAttributes;
 import org.jboss.forge.addon.ui.result.Result;
 import org.jboss.forge.addon.ui.result.Results;
 
-import javax.inject.Inject;
-
-import static java.util.Arrays.asList;
-
 /**
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
  */
 public class SetupVertxCommand extends AbstractVertxCommand {
 
-  @Inject
-  @WithAttributes(shortName = 'v', label = "Vert.x version", type = InputType.DROPDOWN)
-  private UISelectOne<String> version;
+    @Inject
+    @WithAttributes(shortName = 'v', label = "Vert.x version", type = InputType.DROPDOWN)
+    private UISelectOne<String> vertxVersion;
 
-  @Override
-  public void initializeUI(UIBuilder uiBuilder) throws Exception {
-    uiBuilder.add(version);
+    @Override
+    public void initializeUI(UIBuilder uiBuilder) throws Exception {
+	uiBuilder.add(vertxVersion);
 
-    //Version default and values
-    version.setDefaultValue(() -> facet.getDefaultVertxVersion());
-    version.setValueChoices(() -> asList("3.0.0", "3.1.0", "3.2.1", "3.3.2", "3.3.3"));
-  }
+	// Version default and values
+	vertxVersion.setDefaultValue(() -> facet.getDefaultVertxVersion());
+	vertxVersion.setValueChoices(() -> asList("3.0.0", "3.1.0", "3.2.1", "3.3.2", "3.3.3"));
+    }
 
-  @Override
-  public Result execute(UIExecutionContext uiExecutionContext) throws Exception {
-    facet.setVertxVersion(version.getValue());
-    facet.setFaceted(getSelectedProject(uiExecutionContext.getUIContext()));
-    facet.install();
-    return Results.success("vert.x project created successfully");
-  }
+    @Override
+    public Result execute(UIExecutionContext uiExecutionContext) throws Exception {
+	facet.setVertxVersion(vertxVersion.getValue());
+	facet.setFaceted(getSelectedProject(uiExecutionContext.getUIContext()));
+	facet.install();
+	return Results.success("vert.x project created successfully");
+    }
 
-  @Override
-  public String name() {
-    return "vertx-setup";
-  }
+    @Override
+    public String name() {
+	return "vertx-setup";
+    }
 }
